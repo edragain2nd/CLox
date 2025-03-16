d6946743fe73e8fc6b25043326cf56a05592cccb
edragain <1372959677@qq.com>
2025-03-16 21:59:12 +0800

    complete ch19

--- vm.h
+++ vm.h
@@ -10,12 +10,14 @@ typedef struct {
     uint8_t* ip;
     Value stack[STACK_MAX];
     Value* stackTop;
+    Obj* objects;
 } VM;
 typedef enum {
     INTERPRET_OK,
     INTERPRET_COMPILE_ERROR,
     INTERPRET_RUNTIME_ERROR
   } InterpretResult;
+extern VM vm;
 void initVM();
 void freeVM();
 InterpretResult interpret(const char* source);
