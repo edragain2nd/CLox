cc7934ec4e686f5a20d04f73a5f370c7a14f868e
edragain <1372959677@qq.com>
2025-03-17 18:36:18 +0800

    complete ch27

--- object.c
+++ object.c
@@ -21,6 +21,11 @@ static Obj* allocateObject(size_t size, ObjType type) {
 #endif
     return object;
 }
+ObjClass* newClass(ObjString* name) {
+    ObjClass* klass = ALLOCATE_OBJ(ObjClass, OBJ_CLASS);
+    klass->name = name;
+    return klass;
+}
 ObjClosure* newClosure(ObjFunction* function) {
     ObjUpvalue** upvalues = ALLOCATE(ObjUpvalue*,
                                    function->upvalueCount);
@@ -40,6 +45,12 @@ ObjFunction* newFunction() {
     function->name = NULL;
     initChunk(&function->chunk);
     return function;
+}
+ObjInstance* newInstance(ObjClass* klass) {
+    ObjInstance* instance = ALLOCATE_OBJ(ObjInstance, OBJ_INSTANCE);
+    instance->klass = klass;
+    initTable(&instance->fields);
+    return instance;
 }
 ObjNative* newNative(NativeFn function) {
     ObjNative* native = ALLOCATE_OBJ(ObjNative, OBJ_NATIVE);
@@ -101,11 +112,18 @@ static void printFunction(ObjFunction* function) {
 }
 void printObject(Value value) {
     switch (OBJ_TYPE(value)) {
+        case OBJ_CLASS:
+            printf("%s", AS_CLASS(value)->name->chars);
+        break;
         case OBJ_CLOSURE:
             printFunction(AS_CLOSURE(value)->function);
         break;
         case OBJ_FUNCTION:
             printFunction(AS_FUNCTION(value));
+        break;
+        case OBJ_INSTANCE:
+            printf("%s instance",
+                   AS_INSTANCE(value)->klass->name->chars);
         break;
         case OBJ_NATIVE:
             printf("<native fn>");
